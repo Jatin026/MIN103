f2a8746130e3658003f4813a7d2a679b107034d3
Jatin <26.jatinhanda@gmail.com>
2022-01-28 15:38:04 +0530

    Updated

--- tut7/q5.cpp
+++ tut7/q5.cpp
@@ -1,27 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-double interpolate(double x[], double y[], double xp , const int n){
-    double yp=0,count=0;
-    for (int i = 0; i < n-1; i++)
+double interpolate(double x[], double y[], double xp, const int n)
+{
+    double yp = 0, count = 0;
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = i+1; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            yp+=((y[j]-y[i])/(x[j]-x[i]))*(xp-x[i])+y[i];
-            count++;
+            if (x[i] < xp && x[j] > xp)
+            {
+                yp = ((y[j] - y[i]) / (x[j] - x[i])) * (xp - x[i]) + y[i];
+                count++;
+            }
         }
-        
+
+        if (yp == 0)
+        {
+            cout << "ExtraPolation not allowed ";
+            exit(0);
+        }
+        else
+        {
+            return yp / count;
+        }
     }
-    return (yp/count);
 }
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    double x[n],y[n],xp;
-    cin>>xp;
+    cin >> n;
+    double x[n], y[n], xp;
+    cin >> xp;
     for (int i = 0; i < n; i++)
     {
-        cin>>x[i]>>y[i];
+        cin >> x[i] >> y[i];
     }
-    cout<<"The Interpolated value of y is : "<<interpolate(x,y,xp,n);
+    cout << "The Interpolated value of y is : " << interpolate(x, y, xp, n);
     return 0;
 }
