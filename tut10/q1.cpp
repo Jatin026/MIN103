d056c96c9cd882398341ba4d85d20cd8c4bf6203
Jatin <26.jatinhanda@gmail.com>
2022-02-17 23:53:38 +0530

    q1

---
+++ tut10/q1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Room{
+    int length,breadth,height;
+    public:
+        Room(int l , int b, int h){
+            length=l;
+            breadth=b;
+            height=h;
+        }
+        int area(){
+            return (2*length*breadth+2*breadth*height+2*height*length);
+        }
+        int volume(){
+            return (length*breadth*height);
+        }
+};
+int main(){
+    int x,y,z;
+    cin>>x>>y>>z;
+    Room r(x,y,z);
+    cout<<"Area is : "<<r.area()<<"\n";
+    cout<<"Volume is : "<<r.volume();
+    return 0;
+}
